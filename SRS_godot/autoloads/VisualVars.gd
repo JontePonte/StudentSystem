61c4790576bbb1a5033698667d2e9a6943a3111d
JontePonte <john_nordstrand@hotmail.com>
2020-09-16 10:57:10 +0200

    created basic structure for json data files and visuals

---
+++ SRS_godot/autoloads/VisualVars.gd
@@ -0,0 +1,13 @@
+extends Node
+# All variables connected that governs the visuals
+
+
+# Change data file dialogue box
+var changeData_x_marg = 0.05
+var changeData_y_marg = 0.05
+
+
+
+
+func _ready():
+	pass
