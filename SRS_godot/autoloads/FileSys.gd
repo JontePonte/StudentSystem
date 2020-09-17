ced531e234d24fa29dc260cb2b8114722f71289a
JontePonte <john_nordstrand@hotmail.com>
2020-09-17 16:54:05 +0200

    added save and load functions for student data

--- SRS_godot/autoloads/FileSys.gd
+++ SRS_godot/autoloads/FileSys.gd
@@ -10,6 +10,32 @@ func change_active_data_file(new_file_name):
 	app_data_save(data)
 
 
+func student_data_save(data):
+	var path = GlobalVars.activeDataFile
+	var s_data = File.new()
+	if not s_data.file_exists(path):
+		return
+	s_data.open(path, File.WRITE)
+
+	s_data.store_line(to_json(data))
+	s_data.close()
+
+
+func student_data_load():
+	var path = GlobalVars.activeDataFile
+	var s_data = File.new()
+	if not s_data.file_exists(path):
+	    return
+	s_data.open(path, File.READ)
+    # The data is stored in "text"
+	var text = s_data.get_as_text()
+	s_data.close()
+
+    # Unpack information to dict data
+	var data = parse_json(text)
+	return data
+
+
 func app_data_save(data):
 	var path = "res://AppData.json"
 	var app_data = File.new()
