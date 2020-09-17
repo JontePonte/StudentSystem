58720635690d4998d4592614dd076d901dfb5663
JontePonte <john_nordstrand@hotmail.com>
2020-09-17 18:12:39 +0200

    Removed tutorial buttons and prepared code for real buttons

--- SRS_godot/Main.gd
+++ SRS_godot/Main.gd
@@ -9,9 +9,13 @@ var window_size = OS.get_window_size()
 func _ready():  
 	OS.set_window_position(screen_size*0.5 - window_size*0.5)
 	
+	create_subject_buttons()
+
+
+func create_subject_buttons(): 
 	var s_data = FileSys.student_data_load()
 	print(s_data.keys())
-
+	
 	# Make the first button selected so the menu can be navigated with arrows
 	# $Menu/CenterRow/ScrollContainer/Buttons/NewGameButton.grab_focus()
 
