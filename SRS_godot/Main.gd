2b9c4d706798c37fa238080a6ee5eca70fb17a85
JontePonte <john_nordstrand@hotmail.com>
2020-09-17 18:37:07 +0200

    Changed subjects to classes, clearer data structure

--- SRS_godot/Main.gd
+++ SRS_godot/Main.gd
@@ -9,25 +9,25 @@ var window_size = OS.get_window_size()
 func _ready():  
 	OS.set_window_position(screen_size*0.5 - window_size*0.5)
 	
-	create_subject_buttons()
+	create_classes_buttons()
 
 
-func create_subject_buttons(): 
+func create_classes_buttons(): 
 	var s_data = FileSys.student_data_load()
 	print(s_data.keys())
 
 	# Instance menu buttons and make them childs of scroll menu
 	for subject in s_data.keys():
-		var scene = load("res://Buttons/MenuButton.tscn")
-		var subject_button = scene.instance()
+		var scene = load("res://Buttons/ClassButton.tscn")
+		var class_button = scene.instance()
 
-		subject_button.get_node("Label").set_text(subject)
-		subject_button.name = subject
+		class_button.get_node("Label").set_text(subject)
+		class_button.name = subject
 
-		$Menu/CenterRow/ScrollSubjects/Subjects.add_child(subject_button)
+		$Menu/CenterRow/ScrollClass/Classes.add_child(class_button)
 
 	# Make the first button selected so the menu can be navigated with arrows
-	$Menu/CenterRow/ScrollSubjects/Subjects.get_node(s_data.keys()[0]).grab_focus()
+	$Menu/CenterRow/ScrollClass/Classes.get_node(s_data.keys()[0]).grab_focus()
 
 
 func _on_SelectData_pressed():
