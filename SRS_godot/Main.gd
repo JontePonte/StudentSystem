a746295d7e3230bcbaeeefc77a3e4ce9aa8b4ff7
JontePonte <john_nordstrand@hotmail.com>
2020-09-17 16:16:49 +0200

    added label to open file dialogue

--- SRS_godot/Main.gd
+++ SRS_godot/Main.gd
@@ -1,7 +1,13 @@
 extends Control
 
 
+var screen_size = OS.get_screen_size()
+var window_size = OS.get_window_size()
+
+
 func _ready():
+  
+	OS.set_window_position(screen_size*0.5 - window_size*0.5)
 	$Menu/CenterRow/ScrollContainer/Buttons/NewGameButton.grab_focus()
 
 
