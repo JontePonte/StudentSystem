fda254703052e548732775caf58a5293a501ebbc
JontePonte <john_nordstrand@hotmail.com>
2020-09-17 13:35:53 +0200

    Added globalvar autoload and put active data file in it

--- SRS_godot/Main.gd
+++ SRS_godot/Main.gd
@@ -1,8 +1,12 @@
 extends Control
 
 
+var ActiveDataFile = ""
+
+
 func _ready():
 	$Menu/CenterRow/ScrollContainer/Buttons/NewGameButton.grab_focus()
+	ActiveDataFile = GlobalVars.ActiveDataFile
 
 
 func _process(_delta):
--- SRS_godot/autoloads/GlobalVars.gd
+++ SRS_godot/autoloads/GlobalVars.gd
@@ -1,8 +1,26 @@
 extends Node
 # All global variables
 
-var ActiveDataFile = "data2020.json"
+var ActiveDataFile = ""
 
 
 func _ready():
-	pass
+    load_data_file_variables()
+
+
+func load_data_file_variables():
+    var path = "res://SaveData.json"
+
+    var save_data = File.new()
+    if not save_data.file_exists(path):
+        return
+    save_data.open(path, File.READ)
+    # The data is stored in "text"
+    var text = save_data.get_as_text()
+    save_data.close()
+
+    # Unpack information to dict data
+    var data = parse_json(text)
+
+    ActiveDataFile = data.active_data_file
+    print(ActiveDataFile)
