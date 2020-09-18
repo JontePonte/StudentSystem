cba04c7b578b4bd4da20acb8f63d033e81d60163
JontePonte <john_nordstrand@hotmail.com>
2020-09-18 10:09:16 +0200

    Display info keys in student info

--- SRS_godot/Classes/StudentInfo.gd
+++ SRS_godot/Classes/StudentInfo.gd
@@ -7,7 +7,7 @@ func show_info():
 	# set popup menu header to student name
 	$StudentInfoMenu/StudentInfoName.set_text(student_dict.first_name + " " + student_dict.last_name)
 
-
+	print_info_keys(student_dict)
 
 
 func collect_info():
@@ -21,5 +21,19 @@ func collect_info():
 			return student
 
 
-func print_keys():
-	pass
+func print_info_keys(student_dict):
+	for info_key in student_dict.keys():
+		# Don't display tests, comments and assignments in the same list as other info
+		if info_key == "tests":
+			continue
+		elif info_key == "comments":
+			continue
+		elif info_key == "assignemnts":
+			continue
+
+		var scene = load("res://InfoTexts/StudentInfoText.tscn")
+		var info_text = scene.instance()
+
+		info_text.get_node("Label").set_text(info_key)
+
+		$StudentInfoMenu/StudentInfoHBox/StudentInfoKeys.add_child(info_text)
