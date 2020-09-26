2859b16a74e97019e03e52d06037087ed2ee5854
JontePonte <john_nordstrand@hotmail.com>
2020-09-26 15:15:35 +0200

    test name i printed at top of test info and bug fix

--- SRS_godot/Classes/TestInfo.gd
+++ SRS_godot/Classes/TestInfo.gd
@@ -1,6 +1,11 @@
 extends WindowDialog
 
 
+onready var TestName = get_node("Menu/TopVbox/TopHbox/TestName")
+onready var TestProperties = get_node("Menu/Center/TestProperties/TestPropertyScroll/TestPointsProperty")
 
 func show_info():
-	pass
+	var data_dict = FileSys.student_data_load()
+	var test_dict = data_dict.get(GlobalVars.activeClass).get("tests").get(str(GlobalVars.activeTestId))
+
+	TestName.set_text(test_dict.test_name)
