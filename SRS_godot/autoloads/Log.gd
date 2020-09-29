ff1f388827661d8e1eb31c2fb03f3f4e2a699f51
JontePonte <john_nordstrand@hotmail.com>
2020-09-29 20:43:40 +0200

    added debug level to log

--- SRS_godot/autoloads/Log.gd
+++ SRS_godot/autoloads/Log.gd
@@ -1,14 +1,24 @@
 extends Node
 
 
-# Log information message
+# Log debug message
 func info(message):
+	# only print if log is enabled
+    if GlobalVars.enable_log:
+        # Get datetime to dictionary
+        var dt=OS.get_datetime()
+        # Format and print with message
+        print("INFO: ", "%02d:%02d:%02d " % [dt.hour,dt.minute,dt.second], message)
+
+
+# Log debug message
+func debug(message):
 	# only print if log is enabled and debug mode is on
     if GlobalVars.enable_log and GlobalVars.debug_mode:
         # Get datetime to dictionary
         var dt=OS.get_datetime()
         # Format and print with message
-        print("INFO: ", "%02d:%02d:%02d " % [dt.hour,dt.minute,dt.second], message)
+        print("DEBUG: ", "%02d:%02d:%02d " % [dt.hour,dt.minute,dt.second], message)
 
 
 # Log warning message
@@ -18,14 +28,14 @@ func warning(message):
         # Get datetime to dictionary
         var dt=OS.get_datetime()
         # Format and print with message
-        print("WARNING: ", "%02d:%02d:%02d " % [dt.hour,dt.minute,dt.second], message)
+        print("WARN: ", "%02d:%02d:%02d " % [dt.hour,dt.minute,dt.second], message)
 
 
 # Log critical message
-func critical(message):
+func error(message):
 	# only print if log is enabled
     if GlobalVars.enable_log:
         # Get datetime to dictionary
         var dt=OS.get_datetime()
         # Format and print with message
-        print("CRITICAL: ", "%02d:%02d:%02d " % [dt.hour,dt.minute,dt.second], message)
+        print("ERROR: ", "%02d:%02d:%02d " % [dt.hour,dt.minute,dt.second], message)
