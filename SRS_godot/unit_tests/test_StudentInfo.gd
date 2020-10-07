24cec3fc308f216cb6563f50767379ab37f75f6d
JontePonte <john_nordstrand@hotmail.com>
2020-10-07 20:55:33 +0200

    fixed orphan problem by adding instances to root

--- SRS_godot/unit_tests/test_StudentInfo.gd
+++ SRS_godot/unit_tests/test_StudentInfo.gd
@@ -1,8 +1,6 @@
 extends "res://addons/gut/test.gd"
 
-
 # Test functions in StudentInfo
-
 
 # ------------------------------------ Calculate percents ------------------------------------
 
@@ -10,11 +8,13 @@ func test_calculate_percents():
 	
 	var StudentInfo = preload("res://Classes/StudentInfo.tscn")
 	var instance = StudentInfo.instance()
+	get_tree().get_root().add_child(instance)
+
 	var result = [4,4,4]
 	var max_points = [10,10,10]
 	var percents = instance.calculate_percents(result, max_points)
-	instance.queue_free()
-	
 	var correct = ["40 %", "40 %", "40 %"]
+
+	instance.queue_free()
 
 	assert_eq(percents, correct)
