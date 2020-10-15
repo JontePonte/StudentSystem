3045baeed058a5a8b16c131a096cf1924bcd2cf6
JontePonte <john_nordstrand@hotmail.com>
2020-10-15 21:11:31 +0200

    added grade text check to assignment in student info

--- SRS_godot/InfoTexts/AssignmentTextEdit.gd
+++ SRS_godot/InfoTexts/AssignmentTextEdit.gd
@@ -2,3 +2,10 @@ extends VBoxContainer
 
 
 var key_name = ""
+
+
+func _on_GradeLetter_text_changed(text_input):
+    var text_corrected = AuxFunc.remove_invalid_characters_from_grade(text_input)
+    if text_input != text_corrected:
+        Log.info("Removed invalid character(s) %s --> %s" % [text_input, text_corrected])	
+        $AssignmentInfo/GradeHBox/GradeLetter.set_text(text_corrected)
