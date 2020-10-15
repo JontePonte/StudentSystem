b12b029923dfff81997b247b57a96222c9d879cb
JontePonte <john_nordstrand@hotmail.com>
2020-10-15 21:16:33 +0200

    added text check for assignment comments in both student info and assignmet info

--- SRS_godot/InfoTexts/AssignmentInfoStudentResult.gd
+++ SRS_godot/InfoTexts/AssignmentInfoStudentResult.gd
@@ -4,6 +4,13 @@ extends HBoxContainer
 var key_name = ""
 
 
+func _on_Comment_text_changed(text_input):
+    var text_corrected = AuxFunc.remove_invalid_characters_from_text(text_input)
+    if text_input != text_corrected:
+        Log.info("Removed invalid character(s) %s --> %s" % [text_input, text_corrected])	
+        $Comment.set_text(text_corrected)
+
+
 func _on_Grade_text_changed(text_input):
     var text_corrected = AuxFunc.remove_invalid_characters_from_grade(text_input)
     if text_input != text_corrected:
